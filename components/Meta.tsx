7a5b05ab5a5660a7b1e6259b8a041740c382556d
Quinn, Brendan <Brendan.Quinn@nextgearcapital.com>
2018-12-14 16:38:56 -0500

    🛀 clean up meta

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,8 +4,6 @@ export const Meta: React.SFC<{}> = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/static/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-    <title>Sick Fits!</title>
+    <title>Silly Shield</title>
   </Head>
 );
