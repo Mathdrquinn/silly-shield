f48a10ba830c894b5d344ff6762c35edf8a00dd3
Quinn, Brendan <Brendan.Quinn@nextgearcapital.com>
2018-12-14 16:45:39 -0500

    🔗 Link Logo to home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { Nav } from './Nav';
 export const Header = () => (
   <header style={{ border: '1px solid #e4e', padding: '1em' }}>
     <div className="bar">
-      <a href="">Silly Shield</a>
+      <a href="/">Silly Shield</a>
       <Nav />
     </div>
   </header>
